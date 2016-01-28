23737416ddd767e74840e0eb6d28a2acd39f5751
Jibsgrl <jbgariel@gmail.com>
2016-01-28 17:32:44 +0100

    Update ReadSensorData.ino

--- firmware/examples/ReadSensorData.ino
+++ firmware/examples/ReadSensorData.ino
@@ -4,13 +4,15 @@
 I2CSensorParticle sensor;
 
 void setup() {
+  Particle.publish("setup", "Start setup", 300, PRIVATE);
   Serial.begin(9600);
   
   sensor.begin(); // reset sensor
+  Particle.publish("setup", "sensor.begin() done", 300, PRIVATE);
   delay(1000); // give some time to boot up
   String address = String(sensor.getAddress());
   String version = String(sensor.getVersion());
-
+  
   // publish sensors info
   Particle.publish("setup", "I2C Soil Moisture Sensor Address: " + address, 300, PRIVATE);
   Particle.publish("setup", "Sensor Firmware version: " + version, 300, PRIVATE);
